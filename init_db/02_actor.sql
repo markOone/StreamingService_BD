225e69050bf8f7ba337430ebdbbeb4b4bd534a61
markOone <98772113+markOone@users.noreply.github.com>
2025-10-12 19:17:22 +0300

    fix: more data and data fixes

--- init_db/02_actor.sql
+++ init_db/02_actor.sql
@@ -2,4 +2,11 @@ INSERT INTO actor (name, surname, biography) VALUES
 ('Leonardo', 'DiCaprio', 'American actor known for Titanic and Inception.'),
 ('Samuel', 'Jackson', 'American actor and producer, Pulp Fiction, Avengers.'),
 ('Scarlett', 'Johansson', 'American actress, known for Lucy and Avengers.'),
-('Tom', 'Hanks', 'American actor, Forrest Gump, Saving Private Ryan.');
+('Tom', 'Hanks', 'American actor, Forrest Gump, Saving Private Ryan.'),
+('Michael', 'Keaton', 'Is an American actor.'),
+('Val', 'Kilmer', 'Was an American actor.'),
+('George', 'Clooney', 'Is an American actor and filmmaker. Known for his leading man roles on screen in both blockbuster and independent films.'),
+('Christian', 'Bale', 'Is an English actor. Known for his versatility and physical transformations for his roles, he has been a leading man in films of several genres.'),
+('Ben', 'Affleck', 'Is an American actor and filmmaker.'),
+('Robert', 'Pattinson', 'is an English actor. Known for starring in both major studio productions and independent films, he often portrays eccentric characters across a diverse range of genres.');
+
--- init_db/04_movie.sql
+++ init_db/04_movie.sql
@@ -2,4 +2,13 @@ INSERT INTO movie (title, description, year, rating, director_id) VALUES
 ('Inception', 'A mind-bending thriller about dreams within dreams.', 2010, 8.8, 1),
 ('Jurassic Park', 'Dinosaurs are brought back to life in a theme park.', 1993, 8.1, 2),
 ('Pulp Fiction', 'Interwoven stories of crime in Los Angeles.', 1994, 8.9, 3),
-('Lost in Translation', 'Two lost souls meet in Tokyo.', 2003, 7.7, 4);
+('Lost in Translation', 'Two lost souls meet in Tokyo.', 2003, 7.7, 4),
+('Batman', 'The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker.', 1989, 7.5, 6),
+('Batman Returns', 'Second Batman movie with Michael Keaton.', 1992, 7.1, 6),
+('Batman Forever', '', 1995, 5.4, 5),
+('Batman & Robin', '', 1997, 3.8, 5),
+('Batman Begins', '', 2005, 8.2, 1),
+('Dark Knight', '', 2008, 9.1, 1),
+('The Dark Knight Rises', '', 2012, 8.4, 1),
+('Batman v Superman: Dawn of Justice', '', 2016, 6.4, 7),
+('The Batman', '', 2022, 7.8, 8);
