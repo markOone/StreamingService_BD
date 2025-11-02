07540e3c2da2edc3e61d8e6b34895b153cc5f123
StudentPP1 <mishamakytonin@gmail.com>
2025-11-02 16:27:59 +0200

    feat: add select queries to check changes after crud operations

--- lab3/delete.sql
+++ lab3/delete.sql
@@ -1,11 +1,23 @@
 -- 1) Delete a specific performance by its ID
+SELECT * FROM performance;
+
 DELETE FROM performance
-WHERE performance_id = 1;     
+WHERE performance_id = 1; 
+
+SELECT * FROM performance;
 
 -- 2) Delete a movie from a specific subscription plan
+SELECT * FROM included_movie;
+
 DELETE FROM included_movie
 WHERE movie_id = 1 AND subscription_plan_id = 1; 
 
+SELECT * FROM included_movie;
+
 -- 3) Delete all failed payments made before June 1, 2024
+SELECT * FROM payment;
+
 DELETE FROM payment
 WHERE status = 'FAILED' AND paid_at < '2024-06-01';
+
+SELECT * FROM payment;
--- lab3/update.sql
+++ lab3/update.sql
@@ -1,14 +1,26 @@
 -- 1) Update movie rating
+SELECT * FROM movie;
+
 UPDATE movie
 SET rating = 8.0
 WHERE title = 'Wonder Woman';
 
+SELECT * FROM movie;
+
 -- 2) Change user email
+SELECT * FROM "user";
+
 UPDATE "user"
 SET email = 'eve.a@example.com'
 WHERE email = 'eve@example.com';
 
+SELECT * FROM "user";
+
 -- 3) Deactivate user subscription
+SELECT * FROM user_subscription;
+
 UPDATE user_subscription
 SET status = 'CANCELLED'
 WHERE user_id = 1 AND status = 'ACTIVE';
+
+SELECT * FROM user_subscription;
