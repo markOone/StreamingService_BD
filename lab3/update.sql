81164052d6087479385954caee68a631d090a299
Stepan Kostenich <91179695+arcctg@users.noreply.github.com>
2025-11-11 11:41:05 +0200

    feat: refine SQL queries

--- lab3/update.sql
+++ lab3/update.sql
@@ -1,26 +1,32 @@
 -- 1) Update movie rating
-SELECT * FROM movie;
+SELECT * FROM movie
+WHERE title = 'Inception'
 
 UPDATE movie
 SET rating = 8.0
 WHERE title = 'Inception';
 
-SELECT * FROM movie;
+SELECT * FROM movie
+WHERE title = 'Inception';
 
 -- 2) Change user email
-SELECT * FROM "user";
+SELECT * FROM "user"
+WHERE name = 'Alice' AND surname = 'Smith';
 
 UPDATE "user"
-SET email = 'alice@example.com'
-WHERE email = 'new_alice@example.com';
+SET email = 'new_alice@example.com'
+WHERE email = 'alice@example.com';
 
-SELECT * FROM "user";
+SELECT * FROM "user"
+WHERE name = 'Alice' AND surname = 'Smith';
 
 -- 3) Deactivate user subscription
-SELECT * FROM user_subscription;
+SELECT * FROM user_subscription
+WHERE user_id = 5;
 
 UPDATE user_subscription
 SET status = 'CANCELLED'
-WHERE user_id = 5 AND status = 'ACTIVE';
+WHERE user_id = 5;
 
-SELECT * FROM user_subscription;
+SELECT * FROM user_subscription
+WHERE user_id = 5;
