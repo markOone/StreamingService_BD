ed94df92367fff17f616234bc3baef18199c0dca
StudentPP1 <mishamakytonin@gmail.com>
2025-12-02 23:43:06 +0200

    add init schema migration

--- streaming-service/src/main/java/dev/studentpp1/streamingservice/StreamingServiceApplication.java
+++ streaming-service/src/main/java/dev/studentpp1/streamingservice/StreamingServiceApplication.java
@@ -1,5 +1,7 @@
 package dev.studentpp1.streamingservice;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StreamingServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(StreamingServiceApplication.class, args);
     }
 
